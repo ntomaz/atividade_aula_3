495135d97f74675f869d75ed5d0374bcb43c6204
Nathalia Tomaz <110290817+ntomaz@users.noreply.github.com>
2024-05-03 19:18:09 -0300

    Add files via upload

---
+++ aula_3.py
@@ -0,0 +1,14 @@
+#atividade aula 3
+
+n1 =  int(input('digite o 1° número>>'))
+n2 =  int(input('digite o 2° número>>'))
+
+soma  =  n1 +  n2
+divisao  =  n1/n2
+mult =  n1 * n2
+sub =  n1 - n2
+
+print('Soma ', soma)
+print('Divisão ', divisao)
+print('Multiplicação ', mult)
+print('Subtração ', sub)
